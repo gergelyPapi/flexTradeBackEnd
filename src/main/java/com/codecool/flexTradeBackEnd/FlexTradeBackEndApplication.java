f5d005f55c3c65a88acf9e231baeca8cb4c08caf
Gergely Papi <gergely.papi.cc@gmail.com>
2018-06-26 15:27:07 +0200

    Added spring inicializer files.

---
+++ src/main/java/com/codecool/flexTradeBackEnd/FlexTradeBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.flexTradeBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexTradeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexTradeBackEndApplication.class, args);
+	}
+}
